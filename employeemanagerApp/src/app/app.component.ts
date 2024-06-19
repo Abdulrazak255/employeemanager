b78d228ebe6d734951e7f1ecc8d149b78665817f
Abdul <shadowy.3bd@gmail.com>
2024-06-19 03:14:49 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/be/brussel/employeemanger/model/Employee.java

--- employeemanagerApp/src/app/app.component.ts
+++ employeemanagerApp/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Employee } from './employee';
+import { Employeeservice } from './employee.service';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +12,22 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'employeemanagerApp';
+      public employees : Employee[];
+
+      constructor (private employeeService: Employeeservice ){}
+  
+  public getEmployees() : void{
+  this.employeeService.getEmployees().subscribe (
+  
+       (response : Employee[])=> {
+    this.employees = response ;
+  },
+
+  (error : HttpErrorResponse) => {
+
+    alert (error.message);
+  }
+  
+);  
+  }
 }
